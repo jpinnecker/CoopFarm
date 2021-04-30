1267cb7b67de7c89077853b208b3e2031ecea570
Meersalzeis <kaifrichter@gmail.com>
2021-04-30 22:51:26 +0200

    NetworkManager and Character Controller added

---
+++ Assets/ourScripts/playerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class playerController : NetworkBehaviour
+{
+    private float movespeed = 0.2f;
+
+    // Is outside of camera elsewise
+    public void Start() {
+        transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+    public void Update() {
+        handleMovement();
+    }
+
+    // Movement/Input behaviour
+    void handleMovement()
+    {
+        if (isLocalPlayer) {
+            float x_movement = Input.GetAxis("Horizontal");
+            float y_movement = Input.GetAxis("Vertical");
+            transform.position += new Vector3(x_movement*movespeed, y_movement*movespeed, 0f);
+        }
+    }
+}
