b795a290d7fa883fe6c423eadf4156f2f85f0f14
Stefan Bodenschatz <stefan.bodenschatz@onlinehome.de>
2021-05-01 17:27:03 +0200

    Added PlayerState component.

---
+++ Assets/ourScripts/PlayerState.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerState : NetworkBehaviour
+{
+    private const int seedTypesPerSlot = 5;
+    [SyncVar]
+    string playerName;
+    [SyncVar]
+    NetworkIdentity ownGarden;
+    [SyncVar]
+    NetworkIdentity currentGarden;
+    // Plant Identity -> Time of last watering
+    readonly SyncDictionary<NetworkIdentity, double> wateringCooldowns = new SyncDictionary<NetworkIdentity, double>();
+    // Seed type numbers, 0 is empty, only unlocked slots
+    readonly SyncList<int> seedInventory = new SyncList<int>();
+
+    public override void OnStartServer()
+    {
+        UnlockSeedSlot();
+    }
+    public override void OnStartClient() {
+        wateringCooldowns.Callback += OnWateringCooldownUpdate;
+        seedInventory.Callback += OnInventoryUpdate;
+    }
+
+    private void OnWateringCooldownUpdate(SyncIDictionary<NetworkIdentity, double>.Operation op, NetworkIdentity key, double item)
+    {
+        // TODO: Update visuals
+    }
+
+    private void OnInventoryUpdate(SyncList<int>.Operation op, int itemIndex, int oldItem, int newItem)
+    {
+        // TODO: Update UI
+    }
+
+    [Command]
+    public void CmdSetName(string name)
+    {
+        this.name = name;
+    }
+
+    [Server]
+    public void AssignGarden(NetworkIdentity garden)
+    {
+        ownGarden = garden;
+    }
+    [Command]
+    public void CmdEnterGarden(NetworkIdentity garden)
+    {
+        currentGarden = garden;
+        // TODO: Place player game object in this garden
+    }
+
+    [Command]
+    public void CmdWaterPlant(Plant plant) {
+        var netid = plant.gameObject.GetComponent<NetworkIdentity>() as NetworkIdentity;
+        var time = NetworkTime.time;
+        var cooldownEnd = time - plant.GetWateringCooldown(); // If we haven't watered this plant yet, pretend the cooldown just elapsed.
+        wateringCooldowns.TryGetValue(netid, out cooldownEnd);
+        if(time + NetworkTime.timeStandardDeviation >= cooldownEnd) // Give some wiggle room for comparison to account for time variation
+        {
+            plant.Water();
+            wateringCooldowns[netid] = time + plant.GetWateringCooldown();
+        }
+    }
+
+    [Server]
+    public void UnlockSeedSlot()
+    {
+        seedInventory.Add(0);
+    }
+
+    [Server]
+    public void AwardSeeds()
+    {
+        for(int i = 0; i < seedInventory.Count; i++)
+        {
+            seedInventory[i] = Random.Range(i * seedTypesPerSlot, (i + 1) * seedTypesPerSlot);
+        }
+    }
+
+}
