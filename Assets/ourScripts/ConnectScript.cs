ee944bcf78e9fe672b147a9849f72256b4ce68d5
Meersalzeis <kaifrichter@gmail.com>
2021-05-04 22:45:21 +0200

    Login Scene visually improved

--- Assets/ourScripts/ConnectScript.cs
+++ Assets/ourScripts/ConnectScript.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Mirror;
 
 public class ConnectScript : NetworkBehaviour
 {
 
+    [SerializeField] private TMP_InputField usernameInput;
+    [SerializeField] private TMP_InputField passwordInput;
+    [SerializeField] private TMP_InputField serverAdressInput;
+
     public void OnConnectClick() {
-        Debug.Log("Connect was pressed");
+        string username = usernameInput.text;
+        Debug.Log("Connect was pressed, username is " + username);
     }
+
 }
